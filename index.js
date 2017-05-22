2e98f76132d271d0889002f9112330a2add3f1e4
Thabo David Klass <thabo@192.168.1.43>
2017-05-22 23:59:04 +0200

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-
+var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -19,7 +19,11 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  emailAdapter: SimpleSendGridAdapter({
+    apiKey: process.env.SENDGRID_API_KEY,
+    fromAddress: process.env.SENDGRID_FROM_ADDRESS
+  })
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
